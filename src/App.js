d281dd685210fc89ea451f73f5a2a064b0aa840a
pranto <polasahammedpranto@gmail.com>
2022-05-10 09:32:23 +0600

    banner section added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div>
         <Navbar></Navbar>
         <Routes>
-          <Route path="/home" element={<Home />}></Route>
+          <Route path="/" element={<Home />}></Route>
           <Route path="about" element={<About />}></Route>
         </Routes>
     </div>
