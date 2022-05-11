02ae69caecdc60071d7f38c3f38ac3bc3566b45b
pranto <polasahammedpranto@gmail.com>
2022-05-11 19:28:51 +0600

    appointment banner added

--- src/Pages/Home/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment.js
@@ -10,7 +10,7 @@ const MakeAppointment = () => {
             <div className='flex-1 hidden lg:block'>
                 <img className='mt-[-150px]' src={doctor} alt="" />
             </div>
-            <div className='flex-1 text-white'>
+            <div className='flex-1 px-5 text-white'>
                 <h3 className='text-xl text-primary'>Appointment</h3>
                 <h2 className='text-3xl my-3'>Make an appointment Today</h2>
                 <p className='my-3'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
