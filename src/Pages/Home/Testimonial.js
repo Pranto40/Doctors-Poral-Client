a91d07901146f513603cbf5afa065c8192e99248
pranto <polasahammedpranto@gmail.com>
2022-05-11 14:54:13 +0600

    testimonial section added

---
+++ src/Pages/Home/Testimonial.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import quote from '../../assets/icons/quote.svg';
+import people1 from '../../assets/images/people1.png'
+import people2 from '../../assets/images/people2.png'
+import people3 from '../../assets/images/people3.png'
+import Review from './Review';
+
+const Testimonial = () => {
+    const reviews = [
+        {
+            _id: 1,
+            name: 'Winson Herry',
+            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            img: people1,
+            loaction: 'califonia'
+        },
+        {
+            _id: 2,
+            name: 'Winson Herry',
+            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            img: people2,
+            loaction: 'califonia'
+        },
+        {
+            _id: 3,
+            name: 'Winson Herry',
+            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            img: people3,
+            loaction: 'califonia'
+        },
+    ]
+    return (
+        <section className='my-28'>
+            <div className='flex justify-between items-center'>
+                <div>
+                <h4 className='text-xl text-primary font-bold'>Testimonial</h4>
+                <h2>What Our Patients Says</h2>
+                </div>
+                <div className='w-24 lg:w-48'>
+                    <img src={quote} alt="" />
+                </div>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-3 lg:cols-3 gap-5'>
+                {reviews.map(review => <Review
+                key = {review._id}
+                review = {review}
+                ></Review>)}
+            </div>
+        </section>
+    );
+};
+
+export default Testimonial;
