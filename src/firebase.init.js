d131dcba40206f0a195fe85055a6546ab0cfef6f
pranto <polasahammedpranto@gmail.com>
2022-05-14 13:56:26 +0600

    login authentication added and use react hook form

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,14 +1,16 @@
-import { getAuth } from 'firebase/auth';
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
+  apiKey: "AIzaSyDLCj19EkydoylQFqSh8Aylcki9EwMB0Wo",
+  authDomain: "doctors-portal-8ffa3.firebaseapp.com",
+  projectId: "doctors-portal-8ffa3",
+  storageBucket: "doctors-portal-8ffa3.appspot.com",
+  messagingSenderId: "872686904271",
+  appId: "1:872686904271:web:91faec112b95c75e58e8ba"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 export default auth;
